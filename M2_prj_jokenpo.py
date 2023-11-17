1085bc885562a94c9bf0a0aecb73d486ec7a6fdf
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-11-17 11:02:17 -0300

    First commit with request

---
+++ M2_prj_jokenpo.py
@@ -0,0 +1,18 @@
+#Aluno - Guilherme
+#Neste projeto você desenvolverá o jogo pedra, papel e tesoura.
+# Os jogadores serão o usuário e o computador.
+# O jogo deve iniciar pedindo ao usuário para escolher entre "pedra", "papel"ou "tesoura" e então o computador irá fazer a escolha aleatoriamente,após isso,
+# o jogo deve informar quem venceu.
+# Para recordar:
+
+# Pedra > tesoura
+# Tesoura > Papel
+# Papel > Pedra
+
+# Utilize funções para separar cada funcionalidade do jogo!
+# Dica : Utilize a função
+# random.choice(lista)
+# do pacote
+# random
+# pararealizar a escolha do computador.
+
